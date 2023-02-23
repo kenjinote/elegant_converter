9117f8521b116ea424ca821c8c9db0269b83421d
kenjinote <2605401+kenjinote@users.noreply.github.com>
2023-02-23 09:47:06 +0900

    計算機能追加

--- Source.cpp
+++ Source.cpp
@@ -41,33 +41,60 @@ int Factor(int n, LPWSTR lpszText)
 	}
 }
 
+UINT Calculate(LPCWSTR lpszText) {
+	UINT uNumber = 0;
+	int nSize = lstrlen(lpszText);
+	for (int i = 0;i < nSize;i++) {
+		switch (lpszText[i]) {
+		case L'+':
+			uNumber++;
+			break;
+		case L'-':
+			uNumber--;
+			break;
+		case L'D':
+			uNumber<<=1;
+			break;
+		}
+	}
+	return uNumber;
+}
+
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-	static HWND hButton;
+	static HWND hButton1;
+	static HWND hButton2;
 	static HWND hEdit1;
 	static HWND hEdit2;
 	switch (msg)
 	{
 	case WM_CREATE:
-		hEdit1 = CreateWindowEx(WS_EX_CLIENTEDGE, TEXT("EDIT"), 0, WS_VISIBLE | WS_CHILD | WS_TABSTOP | ES_AUTOHSCROLL | ES_NUMBER, 0, 0, 0, 0, hWnd, (HMENU)1000, ((LPCREATESTRUCT)lParam)->hInstance, 0);
-		hButton = CreateWindow(TEXT("BUTTON"), TEXT("変換"), WS_VISIBLE | WS_CHILD | WS_TABSTOP, 0, 0, 0, 0, hWnd, (HMENU)IDOK, ((LPCREATESTRUCT)lParam)->hInstance, 0);
-		hEdit2 = CreateWindowEx(WS_EX_CLIENTEDGE, TEXT("EDIT"), 0, WS_VISIBLE | WS_CHILD | WS_TABSTOP | ES_AUTOHSCROLL | ES_READONLY, 0, 0, 0, 0, hWnd, (HMENU)1001, ((LPCREATESTRUCT)lParam)->hInstance, 0);
+		hEdit1 = CreateWindowEx(WS_EX_CLIENTEDGE, L"EDIT", L"349", WS_VISIBLE | WS_CHILD | WS_TABSTOP | ES_AUTOHSCROLL | ES_NUMBER, 0, 0, 0, 0, hWnd, (HMENU)1000, ((LPCREATESTRUCT)lParam)->hInstance, 0);
+		hButton1 = CreateWindow(L"BUTTON", L"変換↓", WS_VISIBLE | WS_CHILD | WS_TABSTOP, 0, 0, 0, 0, hWnd, (HMENU)IDOK, ((LPCREATESTRUCT)lParam)->hInstance, 0);
+		hButton2 = CreateWindow(L"BUTTON", L"変換↑", WS_VISIBLE | WS_CHILD | WS_TABSTOP, 0, 0, 0, 0, hWnd, (HMENU)1002, ((LPCREATESTRUCT)lParam)->hInstance, 0);
+		hEdit2 = CreateWindowEx(WS_EX_CLIENTEDGE, L"EDIT", 0, WS_VISIBLE | WS_CHILD | WS_TABSTOP | ES_AUTOHSCROLL, 0, 0, 0, 0, hWnd, (HMENU)1001, ((LPCREATESTRUCT)lParam)->hInstance, 0);
 		break;
 	case WM_SIZE:
 		MoveWindow(hEdit1, 10, 10, 512, 32, TRUE);
-		MoveWindow(hButton,10, 50, 256, 32, TRUE);
+		MoveWindow(hButton1, 10, 50, 251, 32, TRUE);
+		MoveWindow(hButton2, 271, 50, 251, 32, TRUE);
 		MoveWindow(hEdit2, 10, 90, 512, 32, TRUE);
 		break;
 	case WM_COMMAND:
 		if (LOWORD(wParam) == IDOK)
 		{
-			SetWindowText(hEdit2, 0);
 			DWORD dwNumber = GetDlgItemInt(hWnd, 1000, 0, 0);
 			WCHAR szOperatorList[1024] = {};
 			Factor(dwNumber, szOperatorList);
 			wstring s = szOperatorList;
 			reverse(s.begin(), s.end());
 			SetWindowText(hEdit2, s.c_str());
+		}
+		else if (LOWORD(wParam) == 1002) {
+			WCHAR szText[1024];
+			GetWindowText(hEdit2, szText, _countof(szText));
+			UINT dwNumber = Calculate(szText);
+			SetDlgItemInt(hWnd, 1000, dwNumber, FALSE);
 		}
 		break;
 	case WM_CLOSE:
@@ -98,14 +125,16 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
 		szClassName
 	};
 	RegisterClass(&wndclass);
+	RECT rect = { 0, 0, 532, 132 };
+	AdjustWindowRect(&rect, WS_CAPTION | WS_SYSMENU, FALSE);
 	HWND hWnd = CreateWindow(
 		szClassName,
-		TEXT("Window"),
-		WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN,
+		L"エレガントな変換機",
+		WS_CAPTION | WS_SYSMENU,
 		CW_USEDEFAULT,
 		0,
-		CW_USEDEFAULT,
-		0,
+		rect.right - rect.left,
+		rect.bottom - rect.top,
 		0,
 		0,
 		hInstance,
